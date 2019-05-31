e6a36f24eaefe576d0f1d28b9d92bc4bb72cc4ef
harjothkhara <harjoth.khara@gmail.com>
2019-05-31 09:22:13 -0700

    base functionailty of route all requests added to routes

--- actionRouter.js
+++ actionRouter.js
@@ -1,14 +1,56 @@
 const express = require("express");
 const router = express.Router;
+const db = require("./data/helpers/actionModel.js");
 
-router.get("path", async (req, res) => {});
+router.get("/", async (req, res) => {
+    try {
+        const actions = await db.get();
+        actions.status(200).json(actions);
+    } catch(error){
+        res.status(500).json({ message: "error"})
+    }
+});
 
-router.get("path", async (req, res) => {});
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try{
+        const action = db.get(id);
+        if(action){
+            res.status(200).json(action);
+        } else {
+            res.status(404).json({ message: "error"})
+        }
+    } catch(error){
+        res.status(500).json({ message: "error"})
+    }
+});
 
-router.post("path", async (req, res) => {});
+router.post("/", async (req, res) => {
+    const action = req.body;
+    try {
+        db.insert(action);
+    } catch(error){
+        res.status(500).json({ message: "error"})
+    }
+});
 
-router.delete("path", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    try{
+        db.remove(id);
+    } catch(error){
+        res.status(500).json({ message: "error"});
+    }
+});
 
-router.put("path", async (req, res) => {});
+router.put("/:id", async (req, res) => {
+    const { id } = req.params;
+    const action = req.body;
+    try {
+        db.update(id, action);
+    } catch(error) {
+        res.status(500).json({ message: "error"})
+    }
+});
 
 module.exports = router;
--- projectRouter.js
+++ projectRouter.js
@@ -1,14 +1,56 @@
 const express = require("express");
 const router = express.Router;
+const db = require("./data/helpers/projectModel")
 
-router.get("path", async (req, res) => {});
+router.get("/", async (req, res) => {
+    try {
+        const projects = await db.get();
+        projects.status(200).json(projects);
+    } catch(error){
+        res.status(500).json({ message: "error"})
+    }
+});
 
-router.get("path", async (req, res) => {});
+router.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const project = db.get(id);
+        if (project) {
+            res.status(200).json(project);
+        } else {
+            res.status(404).json({ message: "error"})
+        }
+    } catch(error) {
+        res.status(500).json({ message: "error"})
+    }
+});
 
-router.post("path", async (req, res) => {});
+router.post("/", async (req, res) => {
+    const project = req.body;
+    try {
+        db.insert(project);
+    } catch(error){
+        res.status(500).json({ message: "error"})
+    }
+});
 
-router.delete("path", async (req, res) => {});
+router.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    try{
+        db.remove(id);
+    } catch(error){
+        res.status(500).json({ message: "error"})
+    }
+});
 
-router.put("path", async (req, res) => {});
+router.put("/:id", async (req, res) => {
+    const { id } = req.params;
+    const project = req.body;
+    try {
+        db.update(id, project);
+    } catch(error){
+        res.status(500).json({ message: "error"})
+    }
+});
 
 module.exports = router;
